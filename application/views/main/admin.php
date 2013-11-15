bdf72038a568a7dd8011800432c8471d63d58af9
merlinnerd922 <richardchen922@gmail.com>
2013-11-15 16:43:15 -0500

    Mhm

---
+++ application/views/main/admin.php
@@ -0,0 +1,5 @@
+<?php
+echo anchor('main/populate','Populate Database') . "<br />";
+echo anchor('main/delete','Delete Database') . "<br />";
+echo anchor('main/customerInfo', 'List of All Tickets Sold');
+?>
