51b3b5f84ecdf610e5a7e37d1f805c6ad0a44d54
rlplayer <Richard_r.l@hotmail.com>
2013-11-18 16:45:41 -0500

    finals

--- application/views/main/showtimes.php
+++ application/views/main/showtimes.php
@@ -2,7 +2,7 @@
 
 echo "<h1>Showtime List</h1>";
 
-echo anchor('','Back') . "<br />";
+echo anchor('main/admin', 'Return') . "<br />";
 echo "<br/>";
 
 //And if the $site variable is not empty we echo it's content by using the generate method of the table class / library
